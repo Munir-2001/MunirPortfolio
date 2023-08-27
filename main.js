324916fd703e5d612e72522d118c74615120cc95
Munir <munirabbasi2001@gmail.com>
2023-08-27 22:44:01 +0500

    first commit for the project

---
+++ main.js
@@ -0,0 +1,30 @@
+var typed = new Typed(".text", {
+    strings: ["Software Engineer", "Back-end Developer", "Blockchain Enthusiast", "Freelancer"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop: true,
+  });
+
+// const sections = document.querySelectorAll('.section');
+
+// const observerOptions = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 0.5 // Adjust as needed
+// };
+
+// const observer = new IntersectionObserver((entries, observer) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('animate');
+//             observer.unobserve(entry.target);
+//         }
+//     });
+// }, observerOptions);
+
+// sections.forEach(section => {
+//     observer.observe(section);
+// });
+
+
